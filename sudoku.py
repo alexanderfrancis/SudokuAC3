4178cb4e5554d758b464e5f4fb437419d07fdff4
keveniskander <keven.iskander@hotmail.com>
2020-11-05 19:40:48 -0500

    update_domain

--- sudoku.py
+++ sudoku.py
@@ -164,6 +164,62 @@ class Sudoku:
 
         return True
 
+
+    def update_domain(self, row, col):
+        """
+        -------------------------------------------------------
+        Returns if a column is valid.
+        Parameters: self - Matrix
+                    col - column index
+        Return: Boolean - True if no repeat numbers (1-9) exist
+                          in the column
+        -------------------------------------------------------
+        """
+        domain = [1,2,3,4,5,6,7,8,9]
+        visited = []
+        if self.table[row][col].value == 0:
+            for i in range(9):
+                if not self.table[i][col].value == 0 and not self.table[i][col] in visited:
+                    visited.append(self.table[i][col].value)
+            
+            for i in range(9):
+                if not self.table[row][i].value == 0 and not self.table[row][i] in visited:
+                    visited.append(self.table[row][i].value)
+
+            r_index = row
+            c_index = col
+
+            #Find top left index of subsquare
+            r = False
+            c = False
+            while r == False or c == False:
+                if(r == False):
+                    if(r_index%3 == 0):
+                        r = True
+                    else:
+                        r_index -= 1
+
+                if(c == False):
+                    if(c_index%3 == 0):
+                        c = True
+                    else:
+                        c_index -= 1
+
+            for row in range(r_index, r_index+3):
+                for col in range(c_index, c_index+3):
+                    if(not self.table[row][col].value in visited):
+                        if (self.table[row][col].value != 0):
+                            visited.append(self.table[row][col].value)
+            
+            new_visited = []
+            for i in domain:
+                if i not in visited:
+                    new_visited.append(i)
+
+            visited = new_visited
+            self.table[row][col].domain = visited
+            
+
     def find_empty(self):
         """
         -------------------------------------------------------
@@ -204,6 +260,8 @@ def main():
     sud = Sudoku()
     sud.print_table()
     print(sud.is_valid())
+    
+    print(sud.update_domain(0,0))
     # sud.print_domains()
 
 if __name__ == "__main__":
